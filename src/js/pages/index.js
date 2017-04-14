95abff6ff4e5db072f0bd6eb0f88934882a09384
Xiaoyong Yang <xiaoyongyang507@163.com>
2017-04-14 19:13:34 +0800

    增加时间输入组件，左下角消息提示框，修改左侧栏交互效果。

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,8 +1,14 @@
-document.addEventListener('mousemove', function (e) {
+timeInit("hour", "minute");
+input_init("main-input", "todo-list");
+var msg = document.getElementById('message-container');
+msg.addEventListener('click', function() {
 	var timeline = document.getElementById('timeline-container');
-	if (e.clientX < 300) {
-		timeline.className = 'timeline-container-show'
-	} else {
+	timeline.className = 'timeline-container-show';
+	msg.className = 'message-container-hide';
+}, false);
+document.addEventListener('mousemove', function(e) {
+	var timeline = document.getElementById('timeline-container');
+	if(e.clientX > 300) {
 		timeline.className = 'timeline-container-hide';
 	}
 });
